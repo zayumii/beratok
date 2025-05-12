778828ea27ac51517b2fad0dc8172344bf803f2e
zayumii <zayumiiy@gmail.com>
2025-05-13 02:34:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def scrape_twitter_accounts(query, max_results=1000):
         output = result.stdout
         # print(f"Snscrape Output: {output}") # Debugging: Print the raw output
         lines = output.strip().split('\n') # Split output into lines
-        accounts = [json.loads(line) for line in lines if line] # Process each line
+        accounts = [json.loads(line) for line in lines if lines] # Process each line
         return accounts
     except subprocess.CalledProcessError as e:
         st.error(f"Error finding project accounts: Command '{' '.join(e.cmd)}' returned non-zero exit status {e.returncode}.  Output: {e.output}")
